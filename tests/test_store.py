5552e4a9d533a1b0c49a9e4bc1b26bbe215053c6
Ben Aaron <ben@theaarons.com>
2023-03-01 13:26:04 -0800

    whoops need to check bytes

--- tests/test_store.py
+++ tests/test_store.py
@@ -6,15 +6,15 @@ import pytest
 def test_cache_put_and_get(tmp_path):
     """Add and fetch an item"""
     s = store.Store(tmp_path)
-    s["nevada"] = "Carson City"
-    assert s["nevada"] == "Carson City"
+    s["nevada"] = b"Carson City"
+    assert s["nevada"] == b"Carson City"
 
 
 def test_cache_put_get_and_del(tmp_path):
     """Delete and fail to fetch an item"""
     s = store.Store(tmp_path)
-    s["nevada"] = "Carson City"
-    assert s["nevada"] == "Carson City"
+    s["nevada"] = b"Carson City"
+    assert s["nevada"] == b"Carson City"
     del s["nevada"]
     with pytest.raises(KeyError):
         # pylint: disable=pointless-statement
