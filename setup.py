855efb2f6387f97b52f1c55bbc4cf2beaaf1d81c
Ben Aaron <ben@theaarons.com>
2023-03-01 10:46:22 -0800

    setup baseline

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    version="0.0.0",
+    name="pystash",
+    description="A terminal based secret keeper",
+    author="Ben Aaron",
+    license="GPLv3",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": ["pystash=pystash.main:main"],
+    },
+)
