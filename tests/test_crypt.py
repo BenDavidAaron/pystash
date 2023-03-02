709187d41e36ae8ce331f4b46b66a6af0e9498f4
Ben Aaron <ben@theaarons.com>
2023-03-01 16:56:04 -0800

    static methods not first order fxns

--- tests/test_crypt.py
+++ tests/test_crypt.py
@@ -2,12 +2,12 @@ from pystash import crypt
 
 
 def test_make_key():
-    crypt.make_key_from_password("test")
+    crypt.Crypt.make_key_from_password("test")
 
 
 def test_encrypt_and_decrypt():
     data = b"bonk"
     password = "insecure"
 
-    encrypted_data = crypt.encrypt_with_password(data, password)
-    assert crypt.decrypt_with_password(encrypted_data, password) == data
+    encrypted_data = crypt.Crypt.encrypt_with_password(data, password)
+    assert crypt.Crypt.decrypt_with_password(encrypted_data, password) == data
