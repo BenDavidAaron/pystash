0605977d53df25358dc2d56833d0106a473cc607
Ben Aaron <ben@theaarons.com>
2023-03-01 16:53:06 -0800

    DRY it out

--- pystash/crypt.py
+++ pystash/crypt.py
@@ -11,33 +11,6 @@ class KeyDerivationError(Exception):
     pass
 
 
-def make_key_from_password(password: str) -> bytes:
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        iterations=69420,
-        salt=b"NotSecureDoNotUse",
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(bytes(password, encoding="utf-8")))
-    try:
-        Fernet(key)
-    except ValueError as exc:
-        raise KeyDerivationError from exc
-    return key
-
-
-def encrypt_with_password(data: bytes, password: str) -> bytes:
-    f = Fernet(make_key_from_password(password))
-    encrypted_data = f.encrypt(data)
-    return encrypted_data
-
-
-def decrypt_with_password(data: bytes, password: str) -> bytes:
-    f = Fernet(make_key_from_password(password))
-    encrypted_data = f.decrypt(data)
-    return encrypted_data
-
-
 class Crypt:
     def __init__(self, dir_root: pathlib.Path):
         self.dir = pathlib.Path(dir_root) / "crypt"
