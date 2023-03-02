e961e064fbf5a6e6a56490093c8e4c9b26d2410e
Ben Aaron <ben@theaarons.com>
2023-03-01 16:43:00 -0800

    format

--- pystash/config.py
+++ pystash/config.py
@@ -7,6 +7,5 @@ def get_default_root_path() -> str:
     app_data_dir = pathlib.Path(appdirs.user_data_dir("pystash", "bda"))
     return str(app_data_dir)
 
+
 ROOT_PATH = os.environ.get("PYSTASH_ROOT", get_default_root_path())
-
-
