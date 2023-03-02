e02d6f89ed5a65768ddf42e40fc371f619e2889d
Ben Aaron <ben@theaarons.com>
2023-03-01 17:39:32 -0800

    implement init command

--- pystash/main.py
+++ pystash/main.py
@@ -7,6 +7,28 @@ CRYPT = crypt.Crypt(config.ROOT_PATH)
 
 
 @click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=True,
+    help="password to unlock vault.",
+)
+def init(password: str):
+    click.echo("setting up vault...")
+    if CRYPT.exists():
+        click.echo("vault already configured, exiting without changes")
+        return
+    CRYPT.record_key_hash(CRYPT.make_key_from_password(password))
+    click.echo("vault configured, please remember your passkey")
+
+
+@main.command()
 @click.argument(
     "secret",
     type=click.STRING,
@@ -16,31 +38,42 @@ CRYPT = crypt.Crypt(config.ROOT_PATH)
     "--password",
     prompt=True,
     hide_input=True,
-    confirmation_prompt=True,
+    confirmation_prompt=False,
     help="password to unlock vault.",
 )
-def main():
-    pass
-
-
-@main.command()
 def get():
     click.echo("getting secret")
 
 
 @main.command()
-@click.argument("name", type=click.STRING)
-@click.option("-v", type=click.STRING, help="Secret to store")
-@click.option(
-    "-f",
-    type=click.File("r"),
-    help="File to store",
+@click.argument(
+    "secret",
+    type=click.STRING,
+    nargs=1,
 )
-@click.option("-e", type=click.STRING, help="Environment variable to store")
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=False,
+    help="password to unlock vault.",
+)
 def put():
     click.echo("putting secret")
 
 
 @main.command()
+@click.argument(
+    "secret",
+    type=click.STRING,
+    nargs=1,
+)
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=False,
+    help="password to unlock vault.",
+)
 def delete():
     click.echo("deleting secret")
