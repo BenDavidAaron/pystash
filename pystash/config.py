29f31e9746cdf66cdb0ffaa10d9f59543eeeb481
Ben Aaron <ben@theaarons.com>
2023-03-01 16:16:49 -0800

    move root config up one level to allow multiple top level items

--- pystash/config.py
+++ pystash/config.py
@@ -3,9 +3,10 @@ import pathlib
 import appdirs
 
 
-def get_default_store_root() -> str:
+def get_default_root_path() -> str:
     app_data_dir = pathlib.Path(appdirs.user_data_dir("pystash", "bda"))
-    return str(app_data_dir / "secrets")
+    return str(app_data_dir)
+
+ROOT_PATH = os.environ.get("PYSTASH_ROOT", get_default_root_path())
 
 
-STORE_ROOT = os.environ.get("PYSTASH_ROOT", get_default_store_root())
--- pystash/store.py
+++ pystash/store.py
@@ -4,27 +4,27 @@ import os
 
 class Store:
     def __init__(self, dir_root: pathlib.Path):
-        self.dir_root = pathlib.Path(dir_root)
+        self.dir = pathlib.Path(dir_root) / "store"
         self.__init_dir()
 
     def __init_dir(self):
-        self.dir_root.resolve().mkdir(parents=True, exist_ok=True)
+        self.dir.resolve().mkdir(parents=True, exist_ok=True)
 
     def get(self, key: str) -> bytes:
         try:
-            with (self.dir_root / key).open("rb") as f:
+            with (self.dir / key).open("rb") as f:
                 return f.read()
         except FileNotFoundError as exc:
             raise KeyError from exc
 
     def put(self, key: str, val: bytes) -> None:
-        with (self.dir_root / key).open("wb") as f:
+        with (self.dir / key).open("wb") as f:
             f.write(val)
 
     def delete(self, key: str) -> None:
-        with (self.dir_root / key).open("wb") as f:
+        with (self.dir / key).open("wb") as f:
             f.write(os.urandom(1024))
-        (self.dir_root / key).unlink()
+        (self.dir / key).unlink()
 
     def __getitem__(self, key):
         return self.get(key)
