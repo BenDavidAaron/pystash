da69a7ea96c7da4c97d325904d965643e3fae051
Ben Aaron <ben@theaarons.com>
2023-03-01 17:04:10 -0800

    add tests for key hash storage and check

--- tests/test_crypt.py
+++ tests/test_crypt.py
@@ -11,3 +11,15 @@ def test_encrypt_and_decrypt():
 
     encrypted_data = crypt.Crypt.encrypt_with_password(data, password)
     assert crypt.Crypt.decrypt_with_password(encrypted_data, password) == data
+
+
+def test_set_and_check_key(tmp_path):
+    c = crypt.Crypt(tmp_path)
+    c.record_key_hash(b"bonk")
+    assert c.check_key_hash(b"bonk")
+
+
+def test_set_and_check_wrong_key(tmp_path):
+    c = crypt.Crypt(tmp_path)
+    c.record_key_hash(b"bonk")
+    assert not c.check_key_hash(b"donk")
